d3fa3e1255d0beeb60a8caf4e28d109f01d0ec6f
Pritha <pritha0609@gmail.com>
2024-09-02 12:20:42 +0530

    push code

---
+++ 1768_merge_strings_alternately.py
@@ -0,0 +1,8 @@
+"""
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+"""
